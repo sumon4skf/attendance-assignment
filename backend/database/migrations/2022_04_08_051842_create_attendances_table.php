1ae97c911cf933a4398c98841e76ad9a31499ab0
sumon4skf <sumon4skf@gmail.com>
2022-04-11 20:01:56 +0600

    migration correction

---
+++ backend/database/migrations/2022_04_08_051842_create_attendances_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('attendances', function (Blueprint $table) {
+      $table->id();
+      $table->string('month')->nullable();
+      $table->string('date')->nullable();
+      $table->string('day')->nullable();
+      $table->string('emp_id')->nullable();
+      $table->string('emp_name', 255)->nullable();
+      $table->string('department', 255)->nullable();
+      $table->string('first_in_time')->nullable();
+      $table->string('last_out_time')->nullable();
+      $table->string('hours_of_work')->nullable();
+      $table->unsignedInteger('user_id')->nullable();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('attendances');
+  }
+}
